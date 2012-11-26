50cd5e73d148a890d59168a4ee898d9cb7990e9d
yuchih <yuchih@2be.com.tw>
2012-11-26 18:43:13 +0800

    Remove model album InputFilterAwareInterface

--- module/Album/src/Album/Model/Album.php
+++ module/Album/src/Album/Model/Album.php
@@ -3,7 +3,6 @@ namespace Album\Model;
 
 use Zend\InputFilter\Factory as InputFactory;
 use Zend\InputFilter\InputFilter;
-use Zend\InputFilter\InputFilterAwareInterface;
 use Zend\InputFilter\InputFilterInterface;
 
 class Album
